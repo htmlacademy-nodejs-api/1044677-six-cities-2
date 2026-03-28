48f2fdf474697a6ae9adf5a7a3e167fceb675d1a
Eldar Dusmuratov <eldardusmuratov@gmail.com>
2026-03-28 04:04:34 +0300

    feat: Добавит поля _id и id. WIP

--- src/shared/modules/user/user.entity.ts
+++ src/shared/modules/user/user.entity.ts
@@ -1,6 +1,10 @@
 import { User } from '../../types/index.js';
 import { defaultClasses, getModelForClass, prop } from '@typegoose/typegoose';
 
+// eslint-disable-next-line @typescript-eslint/no-unsafe-declaration-merging
+export interface UserEntity extends defaultClasses.Base {}
+
+// eslint-disable-next-line @typescript-eslint/no-unsafe-declaration-merging
 export class UserEntity extends defaultClasses.TimeStamps implements User {
   @prop({ required: true })
   public name: string;
